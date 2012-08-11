9ab108014e107519ffd9ecd903970b2ad062e9f2
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2012-08-11 14:54:24 +0900

    fix memory leak

--- dabloomd.php
+++ dabloomd.php
@@ -4,22 +4,21 @@ require "debug_timer.php";
 $bloom = new Dablooms(100000, 0.05, "/tmp/dab.bin", 0);
 
 $parsers = array();
-$nreads = array();
-$tcp = uv_tcp_init();
+$nreads  = array();
+$tcp     = uv_tcp_init();
 
 uv_tcp_bind($tcp, uv_ip4_addr("127.0.0.1", 1224));
 uv_listen($tcp, 100, function ($server, $status) use (&$bloom, &$parsers, &$nreads) {
     $client = uv_tcp_init();
     uv_accept($server, $client);
     $parsers[(int)$client] = memcache_parser_init();
-    $nreads[(int)$client] = 0;
+    $nreads[(int)$client]  = 0;
 
     uv_read_start($client, function($client, $nread, $buffer) use (&$bloom, &$parsers, &$nreads) {
         if ($nread < 0) {
-            var_dump(uv_strerror(uv_last_error()));
             unset($parsers[(int)$client]);
             unset($nreads[(int)$client]);
-            uv_shutdown($client, function($client){
+            uv_shutdown($client, function($client) {
                 uv_close($client);
             });
             return;
@@ -32,12 +31,8 @@ uv_listen($tcp, 100, function ($server, $status) use (&$bloom, &$parsers, &$nrea
             return;
         }
         
-        if (strpos($buffer, "get") === 0) {
-            list($co,$m) = explode("\r\n",$buffer,2);
-            $buffer = join("\r\n",array(trim($co),$m));
-        }
-        
-        $ret = memcache_parser_execute($parsers[(int)$client], $buffer, $nreads[(int)$client], function($command, $key, $options) use (&$bloom, $client){
+        $ret = memcache_parser_execute($parsers[(int)$client], $buffer, $nreads[(int)$client],
+         function($command, $key, $options) use (&$bloom, $client, &$nreads, &$parsers) {
             switch($command){
                 case "get":
                     if($bloom->check($key)) {
@@ -59,17 +54,15 @@ uv_listen($tcp, 100, function ($server, $status) use (&$bloom, &$parsers, &$nrea
                     });
                     break;
                 case "set":
-                    // this causes false positive. don't check.
-                    //if(!$ret = $bloom->check($key)) {
-                        $bloom->add($key, (int)$options['data']);
-                    //} else {
-                    //    fprintf(STDOUT, "damepo %s\n", $key);
-                    //    var_dump($ret);
-                    //}
+                    $bloom->add($key, (int)$options['data']);
                     uv_write($client, "STORED\r\n",function($client){
                     });
                     break;
                 case "quit":
+                    unset($parsers[(int)$client]);
+                    unset($nreads[(int)$client]);
+                    uv_close($client);
+                    break;
                 default:
                     fprintf(STDOUT, $buffer);
                     uv_close($client);
@@ -77,24 +70,14 @@ uv_listen($tcp, 100, function ($server, $status) use (&$bloom, &$parsers, &$nrea
             }
         });
         
-        if ($ret > 0) {
+        if ($ret > 0 && isset($nreads[(int)$client])) {
             $nreads[(int)$client] = 0;
-        } else {
-/*
-            for($i = 0;$i<strlen($buffer);$i++) {
-                fprintf(STDOUT, '"' . $buffer[$i] . '"   => ' . ord($buffer[$i]) . PHP_EOL);
-            }
-*/
-            if (strpos($buffer, "quit") === 0) {
-                unset($parsers[(int)$client]);
-                unset($nreads[(int)$client]);
-                uv_close($client);
-            } else {
-                uv_write($client, "ERROR\n",function($client){
-                });
-                $nreads[(int)$client] = 0;
-            }
-            //uv_close($client);
+        } else if ($ret > 0) {
+            // nothing todo.
+        } else {    
+            $nreads[(int)$client] = 0;
+            uv_write($client, "ERROR\n",function($client){
+            });
         }
     });
 });
